633cc8626536197ab84927a5ef53b742c0f65135
vandanarenjith <107905222+vandanarenjith@users.noreply.github.com>
2022-06-21 09:45:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready (function()
+{
+    
+   $('.menu-button').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-button i').toggleClass("active");
+   })
+
+})
